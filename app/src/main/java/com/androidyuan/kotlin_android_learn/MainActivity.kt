ba44464028c1b8706bb4ec5aa4300a5c01d29181
weizongwei5 <weizongwei5@gmail.com>
2017-05-18 19:46:32 +0800

    view绑定点击事件

--- app/src/main/java/com/androidyuan/kotlin_android_learn/MainActivity.kt
+++ app/src/main/java/com/androidyuan/kotlin_android_learn/MainActivity.kt
@@ -2,13 +2,17 @@ package com.androidyuan.kotlin_android_learn
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
 import android.view.ViewGroup
 
 import com.androidyuan.kotlin_android_learn.core.CoreLogger
 import com.androidyuan.kotlin_android_learn.model.User
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,View.OnClickListener {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +24,24 @@ class MainActivity : AppCompatActivity() {
 
         message.text = "被kotlin设置的TextView."
 
-
-        CoreLogger.getLogger("MainActivityXXXX").d(user.name)
+        message.setOnClickListener(this)
 
 
     }
+
+
+    override fun onClick(v: View?) {
+
+        if(v!=null) {
+            d("被点击viewId:"+ v.id)
+        }
+    }
+
+
+
+
+    fun d(name: String) {
+        CoreLogger.getLogger("MainActivityXXXX").d(name)
+    }
+
 }
